60bd0317f6f45a6c4ef6556b4eea3db00fdd0a87
adlaljhwry4-creator <adlaljhwry4@gmail.com>
2026-07-10 19:13:13 +0000

    First commit

--- Main.lua
+++ Main.lua
@@ -6,3 +6,22 @@ for i = 1, 100 do
 end
 
 print("Finished!")
+local gui = script.Parent
+local background = gui.Background
+
+local text = background.LoadingText
+local bar = background.LoadingBar.Progress
+
+for i = 1,100 do
+    text.Text = "Loading... "..i.."%"
+
+    bar.Size = UDim2.new(i/100,0,1,0)
+
+    task.wait(0.03)
+end
+
+text.Text = "Finished!"
+
+task.wait(1)
+
+gui.Enabled = false
